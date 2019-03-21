abd35093c36e88ea0df5baf0135490c0d7cecf0e
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-03-21 21:06:19 +0530

    Footer modified

--- src/Footer.js
+++ src/Footer.js
@@ -4,13 +4,13 @@ export default () => (
   <footer className="footer">
     <div className="footer-area">
       <ul className="footer-left">
-        <li>Node</li>
-        <li>Express</li>
-        <li>MySQL</li>
+        <li>GraphQL</li>
+        <li>ReactJS</li>
+        <li>Redux</li>
       </ul>
       <ul className="footer-right">
         <li>By <a target="_blank" rel="noopener noreferrer" href="https://zonayed.me">Zonayed Ahmed</a></li>
-        <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/zonayedpca/nodeMySQLPhonebook/">Source</a></li>
+        <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/zonayedpca/nodeMySQLPhonebookClient">Source</a></li>
       </ul>
     </div>
   </footer>
